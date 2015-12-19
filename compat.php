43135012e0d25fdbcc874c5bc69f570416f1e274
Kenvix <kenvix@vip.qq.com>
2015-12-19 14:04:23 +0800

    update compat.php

--- compat.php
+++ compat.php
@@ -8,7 +8,26 @@ if(!function_exists('get_option')) {
 }
 
 if(!function_exists('add_action')) {
-    function add_action($wp) {
+    function add_action($wp, $func, $other = '') {
+        $wp = str_replace(array(''), '', $wp);
+        if(!empty($wp)) {
+            addAction($wp, $func);
+        }
+    }
+}
 
+if(!function_exists('is_admin')) {
+    function is_admin() {
+        if(ROLE == ROLE_ADMIN) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+if(!function_exists('plugin_basename')) {
+    function plugin_basename($path) {
+        return 'moecdn';
     }
 }
